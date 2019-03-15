983e511518651781d33310fc743e01729b3ee560
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-15 11:35:17 +0100

    test: add get order test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 // Require Third-party Dependencies
 require("dotenv").config();
 const test = require("japa");
-const got = require("got");
 const is = require("@slimio/is");
 const { get } = require("httpie");
 
@@ -51,6 +50,34 @@ test.group("REST Tests", (group) => {
         catch (err) {
             assert.equal(err.statusCode, 500);
             assert.equal(err.data, "Unable to found Business Unit with trigram 'WAL'");
+        }
+    });
+
+    test("/order", async(assert) => {
+        const { data } = await get(new URL("order", DEFAULT_URL).href);
+
+        assert.equal(is.array(data), true);
+        const [id, number, status, lastUpdate] = data.pop();
+        assert.equal(is.number(id), true);
+        assert.equal(is.number(number), true);
+        assert.equal(is.number(status), true);
+        assert.equal(is.number(lastUpdate), true);
+
+        const { data: ret } = await get(new URL(`order/${id}`, DEFAULT_URL).href);
+        assert.equal(is.plainObject(ret), true);
+        assert.equal(ret.id, id);
+        assert.equal(ret.number, number);
+        assert.equal(ret.status, status);
+        assert.equal(ret.lastUpdate, lastUpdate);
+    });
+
+    test("/order (Unable to found order with id)", async(assert) => {
+        try {
+            await get(new URL("order/58525500", DEFAULT_URL).href);
+        }
+        catch (err) {
+            assert.equal(err.statusCode, 500);
+            assert.equal(err.data, "Unable to found order with id 58525500");
         }
     });
 });
