d7740acdb1b9c63aad6ec39c0f7f1fa4cac819eb
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-10 17:01:42 +0200

    chore: add jsdoc to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
+/**
+ * @namespace Utils
+ */
+
+/**
+ * @async
+ * @function qWrap
+ * @memberof Utils#
+ * @param {any} query xdevapi Query
+ * @returns {Promise<any[]>}
+ */
 async function qWrap(query) {
     const rows = [];
     await query.execute((row) => rows.push(row));
@@ -5,6 +16,17 @@ async function qWrap(query) {
     return rows;
 }
 
+/**
+ * @function capitalizeFirstLetter
+ * @description capitalize the first letter of a given string
+ * @memberof Utils#
+ * @param {!string} str string to capitalize
+ * @returns {string}
+ *
+ * @example
+ * const result = capitalizeFirstLetter("hello");
+ * assert.equal("Hello", result);
+ */
 function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
